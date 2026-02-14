ae1eab8c71c9d3ad7b484c4869c268212a398808
paritoshchauhan01 <161223889+paritoshchauhan01@users.noreply.github.com>
2026-02-14 19:01:28 +0530

    remove

--- back/src/routes/general.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/general.controller");
-
-router.get("/cuisine", controller.getByCuisine);
-router.get("/flavor", controller.getByFlavor);
-
-module.exports = router;
--- back/src/routes/newMother.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/newMother.controller");
-
-router.get("/protein", controller.getByProtein);
-router.get("/calories", controller.getByCalories);
-
-module.exports = router;
